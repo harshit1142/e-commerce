d78995792afb1366a361422b76e65d123d02bf20
Aiushi Sengupta <aiushisengupta26@gmail.com>
2024-10-04 00:04:32 +0530

    UI/UX change

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1,22 +1,27 @@
 contributors = [
-  {
-    id: 1,
-    fullname: "Harshit Pandey",
-    username: "https://github.com/harshit1142",
-  },
-  {
-    id: 2,
-    fullname: "Ankur Karmakar",
-    username: "https://github.com/HackesticMedusa",
-  },
-   {
-    id: 3,
-    fullname: "hemanthhhhhh",
-    username: "https://github.com/hemanthhhhhh",
-  },
-  {
-    id: 4,
-    fullname: "Anirudh",
-    username: "https://github.com/anirudh-why",
-  },
-]
+	{
+		id: 1,
+		fullname: 'Harshit Pandey',
+		username: 'https://github.com/harshit1142',
+	},
+	{
+		id: 2,
+		fullname: 'Ankur Karmakar',
+		username: 'https://github.com/HackesticMedusa',
+	},
+	{
+		id: 3,
+		fullname: 'hemanthhhhhh',
+		username: 'https://github.com/hemanthhhhhh',
+	},
+	{
+		id: 4,
+		fullname: 'Anirudh',
+		username: 'https://github.com/anirudh-why',
+	},
+	{
+		id: 5,
+		fullname: 'Aiushi Sengupta',
+		username: 'https://github.com/Aiu26',
+	},
+];
