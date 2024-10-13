96420ea180d6ef3d7348212b363876f5b046efea
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-13 09:03:00 +0530

    Edit the functionality of sign up btn

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Toaster} from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>
 );
 
